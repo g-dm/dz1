2dcd7427fd6e88a26c4a7aff66f9fb05f69b5437
abc123 <abc123@MacBook-Pro-abc123.local>
2016-10-14 19:15:24 +0300

    Initial Commit

---
+++ dz1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dz1
+//
+//  Created by abc123 on 14.10.16.
+//  Copyright © 2016 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dz1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dz1
+//
+//  Created by abc123 on 14.10.16.
+//  Copyright © 2016 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
